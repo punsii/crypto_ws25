ec422de01b14ba60eaa43ce5c9ec9d87536b84f5
Paul Menhart <paul@menhart.net>
2025-10-24 17:50:44 +0200

    implement secret sharing

--- src/sss.py
+++ src/sss.py
@@ -1,43 +1,92 @@
 import random
+from typing import List, Tuple
 
 import pytest
 from sage.all import QQ, PolynomialRing
+
+random.seed(1234)
+RING = PolynomialRing(QQ, "x")
 
 
 def main():
     secret = 12345
 
-    polynom = createSecretPolynom(secret, 3, 1, 100)
+    min_number_to_solve = 2
+    number_of_shares = 10
+    print(f"1.: {createSecretSharing(secret, min_number_to_solve, number_of_shares)=}")
+    secretSharing = createSecretSharing(secret, min_number_to_solve, number_of_shares)
+    shares = secretSharing[0:min_number_to_solve]
+    recovered_secret = restoreSecret(shares)
+    print(f"1.: {(recovered_secret)=}")
 
-    print(polynom)
-    # createSecretPolynom(secret, 2, 3)
+    min_number_to_solve = 5
+    number_of_shares = 20
+    print(f"2.: {createSecretSharing(secret, min_number_to_solve, number_of_shares)=}")
+    secretSharing = createSecretSharing(secret, min_number_to_solve, number_of_shares)
+    shares = secretSharing[0:min_number_to_solve]
+    recovered_secret = restoreSecret(shares)
+    print(f"2.: {(recovered_secret)=}")
+
+    print("")
 
 
-def createSecretPolynom(secret, n, minVal, maxVal):
+def createSecretPolynom(secret: int, minNumberToSolve: int, minVal: int, maxVal: int):
     points = [(0, secret)]
 
-    curve_values = random.sample(range(minVal, maxVal), n + 1)
-    points.extend([(i, curve_values[i]) for i in range(1, n)])
+    curve_values = random.sample(range(minVal, maxVal), minNumberToSolve)
+    points.extend([(i, curve_values[i]) for i in range(1, minNumberToSolve)])
 
-    R = PolynomialRing(QQ, "x")
-    return R.lagrange_polynomial(points)
-
-
-def createSecretSharing(secret, n, m):
-    pass
-    # polynome = createSecretPolynom(secret, n, 1, 10)
-    # for i in range(n):
-    #     print(i)
-    # olynom = R.lagrange_polynomial([(0, 1), (1, 2), (1, 2)])
+    return RING.lagrange_polynomial(points)
 
 
-def restoreSecret(interpolationsPunkte):
-    pass
+def createSecretSharing(secret: int, min_number_to_solve: int, number_of_shares: int):
+    MAX_VALUE = min_number_to_solve * 2**15
+    polynome = createSecretPolynom(
+        secret, min_number_to_solve, minVal=-MAX_VALUE, maxVal=MAX_VALUE
+    )
+    return [(i, polynome(i)) for i in range(1, number_of_shares + 1)]
+
+
+def restoreSecret(interpolationsPunkte: List[Tuple[int, int]]):
+    return RING.lagrange_polynomial(interpolationsPunkte)(0)
+
+
+def test_secret_sharing():
+    for i in range(1000):
+        secret = random.randint(0, 2**15)
+        min_number_to_solve = random.randint(2, 100)
+        number_of_shares = random.randint(min_number_to_solve, min_number_to_solve + 10)
+
+        secretSharing = createSecretSharing(
+            secret, min_number_to_solve, number_of_shares
+        )
+        shares = secretSharing[0:min_number_to_solve]
+        shares = random.sample(secretSharing, min_number_to_solve)
+
+        recovered_secret = restoreSecret(shares)
+        assert recovered_secret == secret
+        print(i)
+
+
+def test_too_few_shares():
+    for _ in range(1000):
+        secret = random.randint(0, 2**15)
+        min_number_to_solve = random.randint(2, 100)
+        number_of_shares = min_number_to_solve - 1
+
+        secretSharing = createSecretSharing(
+            secret, min_number_to_solve, number_of_shares
+        )
+        shares = secretSharing
+
+        recovered_secret = restoreSecret(shares)
+        assert recovered_secret != secret
 
 
 def run_test():
-    pytest.main(["./src/p01.py"])
+    pytest.main(["./src/sss.py", "-vv", "--durations=0"])
 
 
 if __name__ == "__main__":
-    main()
+    run_test()
+    # main()
